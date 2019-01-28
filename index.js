6280a9d533eba4a4f0636ea47cc11402829ed724
duhaizhou <942216863@qq.com>
2019-01-28 20:49:36 +0800

    后台API子系统更新

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 /**
  *小肥牛扫码点餐项目API子系统 
 **/
-const POST=8090;
+const PORT=8090;
 const express=require('express');
+const cors=require('cors');
+const bodyParser=require('body-parser');
+const categoryRouter=require('./routes/admin/category');
+const adminRouter=require('./routes/admin/admin');
+const dishRouter=require('./routes/admin/dish');
 
+
+//创建HTTP应用服务器
 var app=express();
-app.listen(POST,()=>{
-  console.log('server listening '+POST+'........');
-})
+app.listen(PORT,()=>{
+  console.log('server listening:'+PORT);
+  
+});
+//使用中间件CORS
+app.use(cors());
+app.use(bodyParser.json());//
+                          //把application/json格式的请求主体数据解析出来放入req.body属性
+
+//挂载路由器
+app.use('/admin/category',categoryRouter);
+app.use('/admin',adminRouter);
+app.use('/admin/dish',dishRouter);
---
+++ routes/admin/dish.js
@@ -0,0 +1,114 @@
+/**
+ * 菜品相关的路由
+ */
+//创建路由器
+const express=require('express');
+const pool=require('../../pool');
+const router=express.Router();
+module.exports=router;
+
+
+/*
+ * API: GET /admin/dish
+ * 含义：获取所有菜品，类别进行分类
+ * 
+ * 返回值形式：
+ * [
+ *  {cid:1,cname:'...',dishList:[{},{},{}...]},
+ *  {cid:2,cname:'..',dishList:[{},{},{}...]},
+ * ...
+ * ]
+ */
+router.get('/',(req,res)=>{
+  //查询所有的菜品类别
+  pool.query('SELECT cid,cname FROM xfn_category ORDER BY cid',(err,result)=>{
+    if(err) throw err;
+    var categoryList=result;//菜品类别数组
+    var count=0;
+    for(var c of categoryList){
+      count++;
+      pool.query('SELECT * FROM xfn_dish WHERE categoryId=?',c.cid,(err,result)=>{
+        if(err) throw err;
+        console.log(c);
+
+        console.log(result);
+        c.dishList=result;//每一类别的所有菜品列表
+        console.log(c);
+          
+      });
+      // if(count==categoryList.length){
+        
+      //   res.send(categoryList);
+      // }
+    }
+  })
+});
+
+
+/**
+ * API: DELETE /admin/category/:cid
+ * 含义：根据表示菜品编号的路由参数。删除该菜品
+ * 返回值形式：
+ * {code:200,msg:'1 category deleted'}
+ * {code:400,msg:'0 category deleted'}
+ */
+router.delete('/:cid',(req,res)=>{
+  //注意：删除菜品类别之前必须先把属于该菜品类别的菜品的类别编号设置为NULL
+    pool.query('UPDATE xfn_dish SET categoryId=NULL WHERE categoryId=?',req.params.cid,(err,result)=>{
+      if(err) throw err;
+      //至此指定类别的菜品已经修改完毕
+      pool.query('DELETE FROM xfn_category WHERE cid=?',req.params.cid,(err,result)=>{
+        if(err) throw err;
+        //获取DELETE语句在数据库中影响的行数
+        if(result.affectedRows>0)
+          res.send({code:200,msg:"1 category deleted"})
+        else{
+          res.send({code:400,msg:'0 category deleted'});
+        }
+      })
+    })
+  })
+ 
+/**
+ * API: POST /admin/category
+ * 请求主体参数：{cname,'xxx'}
+ * 含义：添加新的菜品类别
+ * 返回值形式：
+ * {code:200,msg:'1 category added',cid:x}
+ * 
+ */
+router.post('/',(req,res)=>{
+  var data=req.body;//形如{cname:'xxx'}
+  pool.query('INSERT INTO xfn_category SET ?',data,(err,result)=>{
+    //注意此处SQL语句的简写
+    if(err) throw err;
+    if(result.affectedRows>0){
+      res.send({code:200,msg:'1 category added'})
+    }else{
+      res.send({code:400,msg:'add category failed'})
+    }
+  })
+})
+/* API:PUT /admin/category
+ * 请求主体参数：{cid:x,cname:'xxx'}
+ * 含义:根据菜品类别编号修改类别
+ * 返回值形式：
+ * {code:200,msg:'1 category modified'}
+ * {code:400,msg:'0 category modified,not exists'}
+ * {code:401,msg:'0 category modified,no modification'}
+ */
+router.put('/',(req,res)=>{
+  var data=req.body;//请求数据{cid:xx,cname:'xx'}
+  //TODO:此处可以对输入数据进行验证
+  pool.query('UPDATE xfn_category SET ? WHERE cid=?',[data,data.cid],(err,result)=>{
+    if(err) throw err;
+    console.log(result);
+    if(result.changedRows>0){//实际改变了一行
+      res.send({code:200,msg:'1 category modified'});
+    }else if(result.affectedRows==0){
+      res.send({code:400,msg:'0 category modified,not exists'});
+    }else if(result.affectedRows==1 && result.changedRows==0){ //影响到了一行，但实际改变了0行--新值和旧值完全一样
+      res.send({code:401,msg:'0 category modifeid,no modification'})
+    }
+  })
+})
